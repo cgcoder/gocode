e603340a62b2feb30d238e470b9e407d747f81bc
cgcoder <gopinath.chand@gmail.com>
2024-12-14 16:23:09 -0800

    file server

---
+++ fileserver/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Args struct {
+	port     uint16
+	username string
+	pass     string
+}
+
+func getArgs() Args {
+	var args Args = Args{
+		port: 8899,
+	}
+	for i := 1; i < len(os.Args); i++ {
+		if os.Args[i] == "-p" {
+			if i+1 < len(os.Args) {
+				i, err := strconv.Atoi(os.Args[i+1])
+				if err != nil {
+					panic("Invalid port number")
+				} else {
+					args.port = uint16(i)
+				}
+			}
+		} else if os.Args[i] == "-u" {
+			if i+2 < len(os.Args) {
+				args.username = os.Args[i+1]
+				args.pass = os.Args[i+2]
+			} else {
+				panic("-u requires username and password")
+			}
+		}
+	}
+	return args
+}
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World!")
+}
+
+func main() {
+	args := getArgs()
+	fmt.Printf("Starting fileserver on port : %d\n", args.port)
+	http.HandleFunc(fmt.Sprintf(":%d", args.port), handler)
+	fmt.Printf("Good Bye!")
+}
